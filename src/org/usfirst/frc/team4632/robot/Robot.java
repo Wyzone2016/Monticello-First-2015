f5f70eae6b707e618859ce01d8da446e16609e44
Wyzone2016 <intothewyzone@gmail.com>
2015-01-08 15:05:10 -0600

    Created everything

---
+++ src/org/usfirst/frc/team4632/robot/Robot.java
@@ -0,0 +1,45 @@
+
+package org.usfirst.frc.team4632.robot;
+
+
+import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ *I LIEK ROBITS BRUHHHH!
+ */
+public class Robot extends SampleRobot {
+	RobotDrive myRobot;
+	Joystick stick;
+
+    public Robot() {
+      
+    	
+    	
+    	
+    }
+
+    public void autonomous() {
+       
+    	
+    	
+    }
+
+    public void operatorControl() {
+       
+    	
+    	
+    	
+    }
+
+  
+    public void test() {
+    	
+    	
+    	
+    	
+    	
+    }
+}
