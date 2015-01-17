04c9edef60f4bbd6c9560ddca93221d1d07e75c4
Wyzone2016 <intothewyzone@gmail.com>
2015-01-17 10:41:51 -0600

    Added Random Crap

--- src/org/usfirst/frc/team4632/robot/Robot.java
+++ src/org/usfirst/frc/team4632/robot/Robot.java
@@ -1,45 +1,43 @@
-
 package org.usfirst.frc.team4632.robot;
 
+import java.util.Random;
 
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 
 /**
- *I LIEK ROBITS BRUHHHH!
+ * I LIEK ROBITS BRUHHHH!
  */
 public class Robot extends SampleRobot {
-	RobotDrive myRobot;
-	Joystick stick;
+	
+	Joystick stick = new Joystick(1);
+//	Talon AssassinsCreed = new Talon(0);
+//	Talon Urhmagerd = new Talon(1);
+	Talon DoUEvenLiftBro = new Talon(2);
+	RobotDrive RoadRage = new RobotDrive(0, 1);
+	
+	public Robot() {
 
-    public Robot() {
-      
-    	
-    	
-    	
-    }
+	}
 
-    public void autonomous() {
-       
-    	
-    	
-    }
+	public void autonomous() {
 
-    public void operatorControl() {
-       
-    	
-    	
-    	
-    }
+	}
 
-  
-    public void test() {
-    	
-    	
-    	
-    	
-    	
-    }
+	public void operatorControl() {
+
+		
+		while (isOperatorControl()) {
+			RoadRage.arcadeDrive(stick);
+			DoUEvenLiftBro.set(stick.getZ());
+		}
+
+	}
+
+	public void test() {
+
+	}
 }
